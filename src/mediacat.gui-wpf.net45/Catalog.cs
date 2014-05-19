4e992f80ce44b12c917f4ab4576456d3954abea6
cyranothedaft <cyranothedev@gmail.com>
2014-05-19 08:47:52 -0600

    initial project creation

---
+++ src/mediacat.gui-wpf.net45/Catalog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mediacat.gui_wpf.net45 {
+   class Catalog {
+   }
+}
